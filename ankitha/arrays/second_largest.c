e4ddd373cdf19e3d83d5e091557cfc6817b3c447
Ankitha1007 <123942675+Ankitha1007@users.noreply.github.com>
2024-12-19 12:24:34 +0530

    Update second_largest.c

--- ankitha/arrays/second_largest.c
+++ ankitha/arrays/second_largest.c
@@ -20,13 +20,13 @@ void  find_second_largest(int *arr, int n)
 
 int main()
 {
-	int array[7] = {2,1,3,5,6,1,67};
+	int array[] = {2,1,3,5,6,1,67};
+	int size = sizeof(array)/sizeof(array[0]);
+	printf("array size: %d\n",size);
 	int i;
-	for(i=0; i < 7; i++)
+	for(i=0; i < size; i++)
 	{
 		printf("original array: %d\n",array[i]);
 	}
-	int size = sizeof(array)/sizeof(array[0]);
-	printf("array size: %d\n",size);
 	find_second_largest(array,size);
 }
