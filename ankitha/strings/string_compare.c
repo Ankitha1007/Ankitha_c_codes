b8018e740d8a2cacb5e63378eb19931f4caecdfa
Ankitha1007 <123942675+Ankitha1007@users.noreply.github.com>
2024-12-22 22:24:09 +0530

    Update string_compare.c

--- ankitha/strings/string_compare.c
+++ ankitha/strings/string_compare.c
@@ -6,34 +6,26 @@ char str_cmp(char *str, char *str1)
 	int l1 = strlen(str);
 	int l2 = strlen(str1);
 	int i,flag=0;
-	if(l1 > l2)
+	if(l1 != l2)
 	{
+	    printf("Strings not same\n");
 		return 0;
 	}
-	for(i=0;str[i]!='\0';i++)
+	for(i=0;i<l1;i++)
 	{
 		if(str[i] != str1[i])
 		{
-			flag =1;
-			
-			break;
+		    printf("Not same\n");
+		    break;
 		}
 	}
-	if(flag == 0)
-	{
-		printf("string is not same\n");
-	}
-	else
-	{
-		printf("String is same\n");
-	}
-
-
+	printf("String is same\n");
+	return 1;
 }
 
 void main()
 {
 	char str[]="ankitha";
-	char str1[]="ankithaaa";
+	char str1[]="ankitha";
 	str_cmp(str,str1);
 }
