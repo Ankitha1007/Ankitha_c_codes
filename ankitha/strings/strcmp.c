9ad86ccd0b8fa37e94bde54f11c6b138081e2a35
Ankitha1007 <123942675+Ankitha1007@users.noreply.github.com>
2025-01-02 11:00:22 +0530

    strcompare.c

--- ankitha/strings/strcmp.c
+++ ankitha/strings/strcmp.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
 #include <string.h>
 
-void main()
+int main() 
 {
-	char str1[100]="mirafra.com";
-	char str2[100]="mirafra.com";
-	int i,j;
-	int flag =0;
-	for(i=0;str1[i]!='\0';i++)
-	{
-		for(j=0;str2[j]!='\0';j++)
-		{
-			if(str1[i] == str2[j])
-			{
-				flag =1;		
-				break;
-			}
-		}
-	}
-	if(flag == 0)
-	{
-		printf("2 strings are same\n");
-	}
-	else
-	{
-		printf("2 strings are not same\n");
-	}
-
-
+   char str[100]="ankitha";
+   char str1[]="ankitha";
+   int i,j;
+   int l1 = strlen(str);
+   int l2 = strlen(str1);
+   if(l1!=l2)
+   {
+       printf("Strings are not same\n");
+       return 1;
+   }
+   
+   for(i=0;i<l1;i++)
+   {
+       if(str[i]!=str1[i])
+       {
+           printf("strings are not same\n");
+           return 1;
+       }
+   }
+   printf("strings are same\n");
 }
