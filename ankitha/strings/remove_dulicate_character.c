1e44660d4fcee3cc3f8956181e032ef3ca761cfe
Ankitha1007 <123942675+Ankitha1007@users.noreply.github.com>
2025-01-02 11:13:00 +0530

    remove_dulicate_character.c

--- ankitha/strings/remove_dulicate_character.c
+++ ankitha/strings/remove_dulicate_character.c
@@ -31,3 +31,28 @@ int main()
 	int len = strlen(str);
 	rem_dup_char(str,len);
 }
+
+/* Without using length 
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[]="ankithaankitha";
+    int i,j,k;
+    for(i=0;str[i]!='\0';i++)
+    {
+        for(j=i+1;str[j]!='\0';j++)
+        {
+            if(str[i] == str[j])
+            {
+                for(k=j;str[k]!='\0';k++)
+                {
+                    str[k] = str[k+1];
+                }
+                j--;
+            }
+        }
+    }
+    printf("%s\n",str);
+}*/
