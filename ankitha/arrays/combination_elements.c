a24002004911227b0d9ec25268e21d38d8905182
Ankitha1007 <123942675+Ankitha1007@users.noreply.github.com>
2025-01-03 15:42:35 +0530

    combination_elements.c

--- ankitha/arrays/combination_elements.c
+++ ankitha/arrays/combination_elements.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
    int arr[5]={1,2,3,4,5};
    int size = sizeof(arr)/sizeof(arr[0]);
    int i,j,k,flag=0;
-   for(i=0;i<5;i++)
+   for(i=0;i<size;i++)
    {
-       for(j=i+1;j<5;j++)
+       for(j=i+1;j<size;j++)
        {
-           for(k=j+1;k<5;k++)
+           for(k=j+1;k<size;k++)
            {
                printf("%d %d %d\n",arr[i],arr[j],arr[k]);
            }
@@ -16,14 +17,15 @@ int main() {
    }
 }
 
-// Combination pairs
-// 1 2 3
-//1 2 4
-//1 2 5
-//1 3 4
-//1 3 5
-//1 4 5
-//2 3 4
-//2 3 5
-//2 4 5
-//3 4 5
+/*Combination pairs output:
+1 2 3
+1 2 4
+1 2 5
+1 3 4
+1 3 5
+1 4 5
+2 3 4
+2 3 5
+2 4 5
+3 4 5
+   */
