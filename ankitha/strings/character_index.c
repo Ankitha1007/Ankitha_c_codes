fa8670e073be75549627489c1db2deb852fb47c5
Ankitha1007 <123942675+Ankitha1007@users.noreply.github.com>
2025-01-03 15:21:59 +0530

    character_index.c

--- ankitha/strings/character_index.c
+++ ankitha/strings/character_index.c
@@ -1,13 +1,14 @@
-// Online C compiler to run C program online
+/* print Character and index of charcter from 1 o/p: a1n2k3i4*/
+
 #include <stdio.h>
 
 int main() {
-   char str[]="anki";
+   char str[]="anki
    int count=0;
    int i;
    for(i=0;str[i]!='\0';i++)
    {
        count++;
-       printf("%d%c",count,str[i]);
+      printf("%c%d",str[i],count);
    }
 }
