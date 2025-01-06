b35a4fd9590958eb119ce0b7b55b3b64c929881a
Ankitha1007 <123942675+Ankitha1007@users.noreply.github.com>
2025-01-06 10:43:16 +0530

    sum_of_digits_each_element.c

--- ankitha/arrays/sum_digits_each_element.c
+++ ankitha/arrays/sum_digits_each_element.c
@@ -18,3 +18,25 @@ void main() {
         printf("%d\n",sum);
     }
 }
+
+/*
+#include <stdio.h>
+void main()
+{
+	int arr[]={1,12,123,1234,12345,123456};
+	int size = sizeof(arr)/sizeof(arr[0]);
+	int i,j;
+	for(i=0;i<size;i++)
+	{
+	    int ones = arr[i]%10;
+	    int tens = (arr[i]/10)%10;
+	    int hundred = (arr[i] /100)%10;
+	    int thousand = (arr[i]/1000)%10;
+	    int ten_thousand = (arr[i]/10000)%10;
+	    int lakh = (arr[i]/100000)%10;
+	    int sum = ones+tens+hundred+thousand+ten_thousand+lakh;
+	    printf("%d\n",sum);
+	    
+	}
+}
+*/
