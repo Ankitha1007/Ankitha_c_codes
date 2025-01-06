c594a0ce7480bc547061e5055a53e42ae50001c7
Ankitha1007 <123942675+Ankitha1007@users.noreply.github.com>
2025-01-06 14:59:27 +0530

    selection_sort.c

--- ankitha/sortings/selection_sort.c
+++ ankitha/sortings/selection_sort.c
@@ -2,13 +2,14 @@
 
 void main()
 {
-	int arr[10]= {6,3,7,2,1,8,9,10,4,5};
+	int arr[]= {6,3,7,2,1,8,9,10,4,5};
+	int size = sizeof(arr)/sizeof(arr[0]);
 	int mid_ind;
 	int i,j,temp;
-	for(i=0;i<10;i++)
+	for(i=0;i<size;i++)
 	{
 		mid_ind = i;
-		for(j=i+1; j<10; j++)
+		for(j=i+1; j<size; j++)
 		{
 			if(arr[j]<arr[mid_ind] )
 			{
@@ -22,7 +23,7 @@ void main()
 			arr[mid_ind] = temp;
 		}
 	}
-	for(i=0;i<10;i++)
+	for(i=0;i<size;i++)
 	{
 		printf("%d\n",arr[i]);
 	}
